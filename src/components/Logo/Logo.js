71428f3f35344a146623c7a52c1942cd3554fd67
nataliia-stef <stefuraknataliia@gmail.comf>
2018-03-29 21:59:09 -0400

    responsive added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         {/* will refer to the path where we store our image */}
         <img src={burgerLogo} alt="TastyBurger" />
     </div>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,18 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
+        {/* <div onClick={props.clicked}>Menu</div> */}
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        
+        {/* navigation on Desktop version will not be displayed on Mobile */}
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
