a42421e5196f48a36bb380179859b6427fb8d012
nataliia-stef <stefuraknataliia@gmail.comf>
2018-04-09 01:58:22 -0400

    additional changes to lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //could be functional component
+    componentWillUpdate() {
+        console.log('OS will update!');
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map( ingKey => {
             return <li key={ingKey}>
-                        <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>: {props.ingredients[ingKey]}
+                        <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>: {this.props.ingredients[ingKey]}
                     </li>
         })
-    return (
-        <Aux>
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients: </p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Total price:  <span style={{'fontWeight': 'bold'}}>${props.totalPrice.toFixed(2)}</span></p>
+            <p>Total price:  <span style={{'fontWeight': 'bold'}}>${this.props.totalPrice.toFixed(2)}</span></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
-};
-
-export default orderSummary;
+        );
+    }
+} 
+   
+   
+export default OrderSummary;
